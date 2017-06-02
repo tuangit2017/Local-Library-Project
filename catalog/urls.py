dd06bd407ce8b15cf84f202799fe80d4aef835e0
tuangit2017 <tuanallejo12@gmail.com>
2017-06-02 14:13:02 -0700

    Add folder catalog and localibrary

---
+++ catalog/urls.py
@@ -0,0 +1,26 @@
+
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^books/$', views.BookListView.as_view(), name='books'),
+	url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+	url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-author-detail'),
+	url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+	url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+	url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+	url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+	#CRUD author
+	url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+	#CRUD book
+	url(r'^book/create/$', views.BookCreate.as_view(), name='book_create'),
+	url(r'^book/(?P<pk>\d+)/update/$', views.BookUpdate.as_view(), name='book_update'),
+    url(r'^book/(?P<pk>\d+)/delete/$', views.BookDelete.as_view(), name='book_delete'),
+
+
+]
